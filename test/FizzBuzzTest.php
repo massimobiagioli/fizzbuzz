93df42a6d52b5c16f9665f6ae8e609a2767e7e3f
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-07-07 07:58:32 +0200

    primo test fizzbuzz

---
+++ test/FizzBuzzTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class FizzBuzzTest extends PHPUnit_Framework_TestCase {
+    
+    function test_class_FizzBuzz() {        
+        $fb = new App\FizzBuzz();
+        
+    }
+        
+}
--- test/dummyTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class DummyTest extends PHPUnit_Framework_TestCase {
-    
-    function test_true_is_true() {        
-        $this->assertTrue(true);
-    }
-        
-}
