525700d90cbaabf42537cd70fc5bedb0a3868e8a
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-07-07 08:22:59 +0200

    correzioni build 22

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -12,9 +12,9 @@ class FizzBuzzTest extends \PHPUnit_Framework_TestCase
      * Controlla se la classe FizzBuzz esiste
      */
     public function testClassFizzBuzzExists()
-    {   
+    {
         $fizzBuzz = new \App\FizzBuzz();
         $this->assertNotNull($fizzBuzz);
     }
-
+    
 }
