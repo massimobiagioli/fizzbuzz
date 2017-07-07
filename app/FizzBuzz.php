ddb7d20fb809176b86a86df1598c7e7c5aea91e8
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-07-07 12:25:37 +0200

    feature pop

--- app/FizzBuzz.php
+++ app/FizzBuzz.php
@@ -14,12 +14,20 @@ class FizzBuzz
      */
     public function process($number)
     {
-        if (($number % 3 === 0) && ($number % 5 === 0)) {
+        if (($number % 3 === 0) && ($number % 5 === 0) && ($number % 7 === 0)) {
+            return 'fizzbuzzpop';
+        } elseif (($number % 3 === 0) && ($number % 7 === 0)) {
+            return 'fizzpop';
+        } elseif (($number % 5 === 0) && ($number % 7 === 0)) {
+            return 'buzzpop';
+        } elseif (($number % 3 === 0) && ($number % 5 === 0)) {
             return 'fizzbuzz';
         } elseif ($number % 3 === 0) {
             return 'fizz';
         } elseif ($number % 5 === 0) {
             return 'buzz';
+        } elseif ($number % 7 === 0) {
+            return 'pop';
         } else {
             return $number;
         }
--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -80,6 +80,15 @@ class FizzBuzzTest extends \PHPUnit_Framework_TestCase
     }
     
     /**
+     * Controlla che in corrispondenza di un numero multiplo di 7 esca 'pop'
+     */
+    public function testNumberMultipleOfSevenIsPop()
+    {
+        $number = 7;
+        $this->assertEquals('pop', $this->fizzBuzz->process($number));
+    }
+    
+    /**
      * Controlla che in corrispondenza di un numero multiplo di 3 e di 5 esca 'fizzbuzz'
      */
     public function testNumberMultipleOfThreeAndFiveIsFizzBuzz()
@@ -89,13 +98,40 @@ class FizzBuzzTest extends \PHPUnit_Framework_TestCase
     }
     
     /**
-     * Simula gioco fizzbuzz con una sequenza di numeri da 1 a 100
+     * Controlla che in corrispondenza di un numero multiplo di 3 e di 7 esca 'fizzpop'
+     */
+    public function testNumberMultipleOfThreeAndSevenIsFizzPop()
+    {
+        $number = 21;
+        $this->assertEquals('fizzpop', $this->fizzBuzz->process($number));
+    }
+    
+    /**
+     * Controlla che in corrispondenza di un numero multiplo di 5 e di 7 esca 'buzzpop'
+     */
+    public function testNumberMultipleOfFiveAndSevenIsBuzzPop()
+    {
+        $number = 35;
+        $this->assertEquals('buzzpop', $this->fizzBuzz->process($number));
+    }
+    
+    /**
+     * Controlla che in corrispondenza di un numero multiplo di 3, 5 e 7 esca 'fizzbuzzpop'
+     */
+    public function testNumberMultipleOfThreeAndFiveAndSevenIsFizzBuzzPop()
+    {
+        $number = 105;
+        $this->assertEquals('fizzbuzzpop', $this->fizzBuzz->process($number));
+    }
+    
+    /**
+     * Simula gioco fizzbuzz con una sequenza di numeri da 1 a 500
      */
     public function testPrintSequence()
     {
-        for ($i = 1; $i <= 100; $i++) {
+        for ($i = 1; $i <= 500; $i++) {
             echo $this->fizzBuzz->process($i);
-            if ($i < 100) {
+            if ($i < 500) {
                 echo ' - ';
             }
         }
