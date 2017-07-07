e8759ae8401b8ed529d78488fbe1c5f7019edf92
vagrant <vagrant@homestead>
2017-07-07 09:26:40 +0000

    aggiunti test

--- app/FizzBuzz.php
+++ app/FizzBuzz.php
@@ -7,5 +7,16 @@ namespace App;
  */
 class FizzBuzz
 {
-
+    /**
+     * Processa numero
+     * @param int $number Numero da processare
+     * @return string Numero processato secondo le regole del gioco
+     */
+    public function process($number)
+    {
+        if ($number === 3) {
+            return 'fizz';
+        }
+        return $number;
+    }
 }
--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -7,13 +7,46 @@ namespace AppTest;
  */
 class FizzBuzzTest extends \PHPUnit_Framework_TestCase
 {
-
+    private $fizzBuzz;
+    
+    
+    protected function setUp() {
+        $this->fizzBuzz = new \App\FizzBuzz();
+    }
+    
     /**
      * Controlla se la classe FizzBuzz esiste
      */
     public function testClassFizzBuzzExists()
     {
-        $fizzBuzz = new \App\FizzBuzz();
-        $this->assertNotNull($fizzBuzz);
+        $this->assertNotNull($this->fizzBuzz);
     }
+    
+    /**
+     * Controlla che in corrispondenza del numero 1 esca '1'
+     */
+    public function testNumberOneIsNumberOne()
+    {
+        $number = 1;
+        $this->assertEquals('1', $this->fizzBuzz->process($number));
+    }
+    
+    /**
+     * Controlla che in corrispondenza del numero 2 esca '2'
+     */
+    public function testNumberTwoIsNumberTwo()
+    {
+        $number = 2;
+        $this->assertEquals('2', $this->fizzBuzz->process($number));
+    }
+    
+    /**
+     * Controlla che in corrispondenza del numero 3 esca 'fizz'
+     */
+    public function testNumberThreeIsFizz()
+    {
+        $number = 3;
+        $this->assertEquals('fizz', $this->fizzBuzz->process($number));
+    }
+    
 }
